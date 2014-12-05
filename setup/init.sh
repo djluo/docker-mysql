46f3207ae6fdbe1d8c6ece7fe4a7ca2a7d5ea2ab
djluo <dj.luo@baoyugame.com>
2014-12-05 21:21:53 +0800

    取消初始化时的root密码

--- setup/init.sh
+++ setup/init.sh
@@ -2,19 +2,18 @@
 
 #echo -n "mysql init: "
 
-PW="Z2AZ3yUMGG8bVG2CWgEi"
+#PW="Z2AZ3yUMGG8bVG2CWgEi"
+PW=""
 HOST=`hostname`
 SOCK="/mysql/logs/mysql.sock"
-User="${User:=mysql}"
 
 /usr/bin/mysql_install_db --datadir=/mysql/data >/dev/null
-chown ${User}.${User} /mysql/{data,log,logs}
 
 /usr/bin/mysqld_safe --socket=${SOCK} >/dev/null &
 
 __wait_sock() {
   local i=1
-  while [ $i -lt 120 ]
+  while [ $i -lt 300 ]
   do
     [ -S "${SOCK}" ] && break
     #echo -n "."
@@ -26,15 +25,16 @@ __wait_sock() {
 __wait_sock
 
 /usr/bin/mysql -uroot -S ${SOCK} <<EOF
-grant all privileges on *.* to root@"${HOST}"   identified by "${PW}";
+#grant all privileges on *.* to root@"${HOST}"   identified by "${PW}";
 grant all privileges on *.* to root@"localhost" identified by "${PW}";
 grant all privileges on *.* to root@"127.0.0.1" identified by "${PW}";
-grant all privileges on *.* to root@"%"         identified by "${PW}";
+#grant all privileges on *.* to root@"%"         identified by "${PW}";
 
 grant shutdown on *.* to shutdown@'localhost';
 grant shutdown on *.* to shutdown@'127.0.0.1';
 
 drop user root@'::1';
+drop user root@"${HOST}";
 delete from mysql.user where user='';
 
 drop database test;
@@ -42,11 +42,11 @@ drop database test;
 flush privileges;
 EOF
 
-/usr/bin/mysqladmin -S ${SOCK} shutdown -uroot -p${PW} >/dev/null
+/usr/bin/mysqladmin -S ${SOCK} -ushutdown shutdown >/dev/null
 #__wait_sock
 
 touch /mysql/data/.init_lock
 
 echo "======================================================"
-echo "The initial password for the mysql(root): ${PW}"
+echo "The initial password for the mysql(root): no password,is empty"
 echo "======================================================"
