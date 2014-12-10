ec1561ceebc89a3143d45483c1897429d7dfe83d
djluo <dj.luo@baoyugame.com>
2014-12-10 10:33:15 +0800

    屏蔽输出,明确创建相同的组,显示锁文件

--- setup/cmd.sh
+++ setup/cmd.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-if [ ! -f "/mysql/data/.init_lock" ];then
+User_Id="${User_Id:=3306}"
+if ! `id -u ${User_Id} >/dev/null 2>&1` ;then
+  /usr/sbin/useradd -U -u ${User_Id} -m -s /bin/false docker
+fi
+
+if [ ! -f "/mysql/data/init_complete" ];then
   /mysql/init.sh
 fi
 
