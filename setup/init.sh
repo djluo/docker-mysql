41d67f058e8bc305581e7ebb45af07f312aabc0d
djluo <dj.luo@baoyugame.com>
2014-12-10 11:29:35 +0800

    完善初始化脚本

--- setup/init.sh
+++ setup/init.sh
@@ -6,15 +6,14 @@
 PW=""
 HOST=`hostname`
 SOCK="/mysql/logs/mysql.sock"
-User_Id="${User_Id:=2019}"
 
-/usr/sbin/useradd -u ${User_Id} -m -s /bin/false docker
+retvar=1
 
-/usr/bin/mysql_install_db --datadir=/mysql/data >/dev/null
-
-chown -R docker.docker /mysql/data /mysql/log /mysql/logs
-
-/usr/bin/mysqld_safe >/dev/null &
+_error() {
+  local msg=$@
+  echo "$msg"
+  exit 127
+}
 
 __wait_sock() {
   local i=1
@@ -27,13 +26,22 @@ __wait_sock() {
   done
   #echo
 }
+
+/usr/bin/mysql_install_db --datadir=/mysql/data >/dev/null || _error "mysql_install_db error?"
+
+chown -R docker.docker /mysql/data /mysql/log /mysql/logs  || _error "chown error?"
+
+/usr/bin/mysqld_safe >/dev/null &
+[ $? -eq 0 ] || _error "start mysql error?"
+
 __wait_sock
 
+[ -S "${SOCK}" ] || _error "not found sock file?"
+
 /usr/bin/mysql -uroot -S ${SOCK} <<EOF
-#grant all privileges on *.* to root@"${HOST}"   identified by "${PW}";
-grant all privileges on *.* to root@"localhost" identified by "${PW}";
-grant all privileges on *.* to root@"127.0.0.1" identified by "${PW}";
-#grant all privileges on *.* to root@"%"         identified by "${PW}";
+#grant all privileges on *.* to root@"localhost";
+#grant all privileges on *.* to root@"127.0.0.1";
+#grant all privileges on *.* to root@"%"       ;
 
 grant shutdown on *.* to shutdown@'localhost';
 grant shutdown on *.* to shutdown@'127.0.0.1';
@@ -42,17 +50,19 @@ drop user root@'::1';
 drop user root@"${HOST}";
 delete from mysql.user where user='';
 
-drop database test;
+#drop database test;
 
 flush privileges;
 EOF
+[ $? -eq 0 ] || _error "change privileges error?"
 
 /usr/bin/mysqladmin -S ${SOCK} -ushutdown shutdown >/dev/null
-#__wait_sock
+[ -S "${SOCK}" ] && _error "stop mysql error?"
 
 chmod 644 /mysql/logs/error.log /mysql/logs/slowquery.log
 chmod 750 /mysql/log /mysql/data
-touch /mysql/data/.init_lock
+
+echo "not delele me!!!" > /mysql/data/init_complete
 
 echo "======================================================"
 echo "The initial password for the mysql(root): no password,is empty"
