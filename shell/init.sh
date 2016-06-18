29701638ce642c22fab7cac49693e68652bbee90
djluo <dj.luo@baoyugame.com>
2016-06-18 11:38:11 +0800

    更新合集

--- shell/init.sh
+++ shell/init.sh
@@ -59,6 +59,7 @@ EOF
 unset xtrab_pw
 
 /usr/bin/mysql -uroot -S ${SOCK} <<EOF
+grant all privileges on *.* to root@"172.17.%";
 grant shutdown on *.* to shutdown@'localhost';
 grant shutdown on *.* to shutdown@'127.0.0.1';
 
